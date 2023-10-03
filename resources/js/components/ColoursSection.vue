d971bba33f57d9f5ab9ae5660db5b4691b7cdbf5
Michael Edes <michaeledes.dev@gmail.com>
2023-10-03 20:39:37 +0100

    refactor: extract colour data into own component

--- resources/js/components/ColoursSection.vue
+++ resources/js/components/ColoursSection.vue
@@ -12,77 +12,21 @@
 </div>
 </template>
 
-import ColoursSectionItem from './ColoursSectionItem.vue';
 
 <script>
 import ColoursSectionItem from './ColoursSectionItem.vue';
+import ColoursData from './data/ColourData.vue'; 
 
 export default {
-    name: "ColourSectionList",
-    components: {
-        ColoursSectionItem
-    },
-    data() {
-        return {
-            colors: [{
-                    name: 'French Grey',
-                    hex: '#D3D3D3'
-                },
-                {
-                    name: 'Foiled White',
-                    hex: '#FFFFFF'
-                },
-                {
-                    name: 'Anthracite Grey',
-                    hex: '#414A4C'
-                },
-                {
-                    name: 'Golden Oak',
-                    hex: '#DAA520'
-                },
-                {
-                    name: 'Schwarzbraun',
-                    hex: '#321414'
-                },
-                {
-                    name: 'Rosewood',
-                    hex: '#674846'
-                },
-                {
-                    name: 'Cream',
-                    hex: '#FFFDD0'
-                },
-                {
-                    name: 'Chartwell Green',
-                    hex: '#7E9B76'
-                },
-                {
-                    name: 'Irish Oak',
-                    hex: '#916E3A'
-                },
-                {
-                    name: 'Duck Egg Blue',
-                    hex: '#C3E0E5'
-                },
-                {
-                    name: 'Off-White',
-                    hex: '#F8F8F8'
-                },
-                {
-                    name: 'Charcoal',
-                    hex: '#36454F'
-                },
-                {
-                    name: 'Blue',
-                    hex: '#0000FF'
-                },
-                {
-                    name: 'Moss Green',
-                    hex: '#4A766E'
-                }
-            ]
-        };
-    }
+  name: "ColourSectionList",
+  components: {
+    ColoursSectionItem
+  },
+  data() {
+    return {
+      colors: ColoursData.data().colors
+    };
+  }
 }
 </script>
 
---
+++ resources/js/components/data/ColourData.vue
@@ -0,0 +1,70 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "ColoursData",
+  data() {
+    return {
+            colors: [{
+                    name: 'French Grey',
+                    hex: '#D3D3D3'
+                },
+                {
+                    name: 'Foiled White',
+                    hex: '#FFFFFF'
+                },
+                {
+                    name: 'Anthracite Grey',
+                    hex: '#414A4C'
+                },
+                {
+                    name: 'Golden Oak',
+                    hex: '#DAA520'
+                },
+                {
+                    name: 'Schwarzbraun',
+                    hex: '#321414'
+                },
+                {
+                    name: 'Rosewood',
+                    hex: '#674846'
+                },
+                {
+                    name: 'Cream',
+                    hex: '#FFFDD0'
+                },
+                {
+                    name: 'Chartwell Green',
+                    hex: '#7E9B76'
+                },
+                {
+                    name: 'Irish Oak',
+                    hex: '#916E3A'
+                },
+                {
+                    name: 'Duck Egg Blue',
+                    hex: '#C3E0E5'
+                },
+                {
+                    name: 'Off-White',
+                    hex: '#F8F8F8'
+                },
+                {
+                    name: 'Charcoal',
+                    hex: '#36454F'
+                },
+                {
+                    name: 'Blue',
+                    hex: '#0000FF'
+                },
+                {
+                    name: 'Moss Green',
+                    hex: '#4A766E'
+                }
+            ]
+        };
+  }
+}
+</script>
