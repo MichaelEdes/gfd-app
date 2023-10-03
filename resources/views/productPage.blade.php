fc362dd43cd7210b89c935b210ba51c80e0fe934
Michael Edes <michaeledes.dev@gmail.com>
2023-10-03 20:35:38 +0100

    chore: final touch ups

--- resources/views/productPage.blade.php
+++ resources/views/productPage.blade.php
@@ -4,9 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>GFD Trading</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600;700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;500;700&display=swap" rel="stylesheet">
